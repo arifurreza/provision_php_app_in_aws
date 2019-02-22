ac7ac05a8430e0627954e2568e11dba69c4b8fb6
Aman <arif.reza@ipay.com.bd>
2019-02-23 05:33:41 +0600

    PHP user registration app

---
+++ my_test_app/api/config/database.php
@@ -0,0 +1,26 @@
+<?php
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "tbwrtwj6tqcfas.cszeqrsni6fa.us-east-2.rds.amazonaws.com";
+    private $db_name = "registration_v1";
+    private $username = "test";
+    private $password = "12345698";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
